4480a891e12f305d0e0e6d787e983deaae51c153
Marcus Carlsson <carlsson.marcus@gmail.com>
2011-11-10 12:10:22 +0100

    views.py: Add support for JSONP.

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,17 +1,28 @@
 import random
+from functools import wraps
 
-from flask import request, jsonify, render_template, abort
+from flask import request, jsonify, abort
 from mongoengine.base import ValidationError
 
 from flaskr import app
 from models import Topic
 
-@app.route('/')
-def main():
-    return render_template('base.html', data='screw this')
+
+def jsonp(f):
+    """Wraps JSONified output for JSONP"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        callback = request.args.get('callback', False)
+        if callback:
+            content = str(callback) + '(' + str(f(*args,**kwargs).data) + ')'
+            return app.response_class(content, mimetype='application/javascript')
+        else:
+            return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route('/api/1/topics/', methods=['GET', 'POST'])
+@jsonp
 def get_or_create_topic():
     if request.method == 'POST':
         topic = request.form.get('topic', None)
@@ -23,8 +34,8 @@ def get_or_create_topic():
                 error = 'Could not validate your data. \
                 Please refer to the API-documentation.')
 
-        return jsonify(success = created,
-            data = [{'id': str(t.id), 'topic': t.topic}])
+        return jsonify(success=created,
+            data=[{'id': str(t.id), 'topic': t.topic}])
 
     elif request.method == 'GET':
         count = request.args.get('count') or 1
@@ -44,10 +55,10 @@ def get_or_create_topic():
                     rand = random.randint(0, total-1)
                 t.append(Topic.objects.all().skip(rand).next())
             if len(t) == 0:
-                return jsonify(success = False, data = [], 
-                        error = 'No topics found.')
+                return jsonify(success=False, data=[], 
+                        error='No topics found.')
         data = [{'id': str(x.id), 'topic': x.topic} for x in t]
-        return jsonify(success = True, data = data)
+        return jsonify(success=True, data=data)
 
     abort(400)
 
